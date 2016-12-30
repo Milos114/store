2f787a578c1caf42ed0dfa71e3ec6d84c4f1ff0d
Harry <milos.stefanovic@cosmicdevelopment.com>
2016-12-30 12:46:13 +0100

    api refactor

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,40 +6,27 @@ use GuzzleHttp\Client;
 
 class ApiController extends Controller
 {
-    protected $username;
-
-    /**
-     * @param  string $username
-     * @return mixed
-     */
     public function gitData($username)
     {
-        $this->username = $username;
-
-        return $this->events()->pluck('type')->map(function ($event) {
-            return $this->eventScores()->get($event, 1);
-        })->sum();
-    }
-
-    /**
-     * @return \Illuminate\Support\Collection
-     */
-    protected function events()
-    {
-        $client = new Client(['base_uri' => 'https://api.github.com/']);
-        return $events = collect(json_decode($client->get("users/$this->username/events")->getBody()));
-    }
-
-    /**
-     * @return \Illuminate\Support\Collection
-     */
-    protected function eventScores()
-    {
-        return $eventScores = collect([
+        $eventScores = collect([
             'PushEvent' => 5,
             'CreateEvent' => 4,
             'IssuesEvent' => 3,
             'CommitCommentEvent' => 2
         ]);
+
+        return $this->events($username)->pluck('type')->map(function ($event) use ($eventScores) {
+            return $eventScores->get($event, 1);
+        })->sum();
+    }
+
+    /**
+     * @param  $username
+     * @return \Illuminate\Support\Collection
+     */
+    protected function events($username)
+    {
+        $client = new Client(['base_uri' => 'https://api.github.com/']);
+        return $events = collect(json_decode($client->get("users/$username/events")->getBody()));
     }
 }
